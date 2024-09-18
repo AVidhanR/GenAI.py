2413fe3ccaa0ff505fc89553e32557ccaf3b0888
AVidhanR <itsvidhanreddy@gmail.com>
2024-09-18 20:39:50 +0530

    Initial commit

---
+++ language_models.py
@@ -0,0 +1,9 @@
+print(
+    '''
+    pip install virtualenv
+    virtualenv nlp_env
+
+    pip install nltk numpy
+    '''
+)
+# Run the above code in your terminal to install the necessary packages.
